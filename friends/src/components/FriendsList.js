e6be6b458e40e1039fca3ea1524e82d8cad6e994
JordanG <jmg236@yahoo.com>
2019-05-16 16:36:21 -0500

    update and delete

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import FriendCard from './FriendCard'
+import AddFriend from './AddFriend'
 
 export default class FriendList extends Component {
     constructor(props) {
@@ -24,16 +25,58 @@ export default class FriendList extends Component {
           });
       }
 
-      render() {
-        return (
-          <div className="friend-list">
-            {this.state.friends.map(friend => (
-              <FriendDetails key={friend.id} friend={friend} />
-            ))}
-          </div>
-        );
-      }
+      addFriend = friend => {
+        axios
+        .post('http://localhost:5000/friends', friend)
+        .then(res => {
+            this.setState({friends: res.data})
+            console.log(res)
+            this.props.history.push('/')
+        })
+        .catch(err => console.log(err))
+
     }
+
+    updateFriend = (updatedFriend) => {
+        axios
+        .put(`http://localhost:5000/friends/${updatedFriend.id}`, updatedFriend)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+
+    deleteFriend = (e,id) => {
+        e.preventDefault();
+        axios
+        .delete(`http://localhost:5000/friends/${id}`)
+        .then(res => {
+            this.setState({friends: res.data})
+            console.log(res)
+            this.props.history.push('/')       
+         })
+        .catch(err => console.log(err))
+    }
+
+    render(){
+      return(
+          <div className= "friendlistwrapper">
+           {this.state.friends.map(friend => ( <div key = {friend.id}>
+              <p>Name: {friend.name} </p>
+              <p>Age: {friend.age} </p>
+              <p>Email: {friend.email} </p>
+              <button onClick={e => this.deleteFriend(e,friend.id)}>X</button>
+           </div>
+  
+           ))}
+          
+          <div>
+          <AddFriend addFriend ={this.addFriend} />
+  
+          </div>
+          </div>
+      )
+  
+  }
+  }
 
     function FriendDetails({ friend }) {
       return (
